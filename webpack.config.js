4433d7898e137a26984761943f9576018e22ee88
Isaah Forrest <isaah.forrest@gmail.com>
2019-02-14 11:16:53 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  node: {
+    net: "empty"
+  },
+  plugins: [new UglifyJsPlugin()],
+  externals: ["react"]
+};
